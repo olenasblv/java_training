8b0652ee82e4cad6705537c15a400f1d2f5f20d7
olenasblv <alenapoda@gmail.com>
2016-09-11 12:38:31 +0300

    Code structure was changed according format settings

--- sandbox/src/main/java/ua/qa/training/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/qa/training/sandbox/MyFirstProgram.java
@@ -2,9 +2,8 @@ package ua.qa.training.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args){
-	
-	System.out.println("Hello, world!");
-}
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 
 }
