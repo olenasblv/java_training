f54912369f9d6c2d7aee084428e23e1d7309ffd9
olenasblv <alenapoda@gmail.com>
2016-09-11 16:21:45 +0300

    Added classes Rectungle and Square

---
+++ sandbox/src/main/java/ua/qa/training/sandbox/Rectangle.java
@@ -0,0 +1,16 @@
+package ua.qa.training.sandbox;
+
+/**
+ * Created by osoboleva on 9/11/2016.
+ */
+public class Rectangle {
+
+  public double a;
+  public double b;
+
+  public Rectangle(double a, double b){
+
+    this.a = a;
+    this.b = b;
+  }
+}
---
+++ sandbox/src/main/java/ua/qa/training/sandbox/Square.java
@@ -0,0 +1,16 @@
+package ua.qa.training.sandbox;
+
+/**
+ * Created by osoboleva on 9/11/2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l){
+
+    this.l = l;  // .l - аттрибут, l- аргумент функции
+  }
+
+
+}
