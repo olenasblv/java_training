8ea7da4a89ea804e001014fd5c532dbb93047989
olenasblv <alenapoda@gmail.com>
2016-09-18 23:04:42 +0300

    Created TestBase class. Transferred common methods there

---
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package ua.qa.training.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() {
+
+    goToGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
