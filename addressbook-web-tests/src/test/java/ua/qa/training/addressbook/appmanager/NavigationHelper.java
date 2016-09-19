fcdf71ee6eaf9308dce2d3a671a84618da108946
olenasblv <alenapoda@gmail.com>
2016-09-19 21:53:57 +0300

    Created HelperBase. Used in Session, Navigation, Group

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/NavigationHelper.java
@@ -6,18 +6,18 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by osoboleva on 9/18/2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
+
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void goToGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
   public void goToAddNewContact() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 }
