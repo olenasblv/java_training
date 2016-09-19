9918a6bd730d74db097477b74ca2ea7a08a20571
olenasblv <alenapoda@gmail.com>
2016-09-19 22:35:01 +0300

    isAlertPresent moved to HelperBase

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/HelperBase.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/HelperBase.java
@@ -1,6 +1,7 @@
 package ua.qa.training.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -21,6 +22,15 @@ public class HelperBase {
     click(locator);
     wd.findElement(locator).clear();
     wd.findElement(locator).sendKeys(text);
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
   }
 
   /*protected void isSelected(By locator) {
