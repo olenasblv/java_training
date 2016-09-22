a7ad3eae16aff4269811ee5234a4ed90fd9ad8a1
Olena Soboleva <alenapoda@gmail.com>
2016-09-22 18:18:06 +0300

    Created EquationTests and Equation classes

---
+++ sandbox/src/main/java/ua/qa/training/sandbox/Equation.java
@@ -0,0 +1,59 @@
+package ua.qa.training.sandbox;
+
+/**
+ * Created by osoboleva on 22.09.2016.
+ */
+public class Equation {
+
+    private double a;
+    private double b;
+    private double c;
+
+    private int n;
+
+    public Equation(double a, double b, double c) {
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        double d = b * b - 4 * a * c;
+
+        if (a != 0) {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
+                n = 1;
+            } else {
+                n = 0;
+            }
+
+        } else if (b != 0) {
+            n = 1;
+        } else if (c != 0) {
+            n = 0;
+        } else {
+            n = -1;
+        }
+    }
+
+
+
+
+
+        /*if(d >0){
+        n = 2;
+    }
+
+     if(d ==0){
+        n = 1;
+    }
+    if(d< 0){
+        n = 0;
+    }*/
+
+
+    public int rootNumber() {
+        return n;
+    }
+}
