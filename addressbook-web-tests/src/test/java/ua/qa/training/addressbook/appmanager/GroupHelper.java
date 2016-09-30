2aad07abbe7ed73c11a3a398d362dd13279a2b8f
olenasblv <alenapoda@gmail.com>
2016-09-30 14:00:06 +0300

    Created the list of Groups

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/GroupHelper.java
@@ -2,8 +2,12 @@ package ua.qa.training.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ua.qa.training.addressbook.model.GroupData;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by osoboleva on 9/18/2016.
@@ -62,5 +66,16 @@ public class GroupHelper extends HelperBase {
 
     public int getGroupCount() {
         return wd.findElements(By.name("selected[]")).size();
+    }
+
+    public List<GroupData> getGroupList() {
+        List<GroupData> groups = new ArrayList<GroupData>();
+        List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements) { // переменная element пробегает по списку elements
+            String name = element.getText(); // из каждого элмента получаем текст, который будет именем группы
+            GroupData group = new GroupData(name, null, null); //создаем объект типа GroupData
+            groups.add(group); // добавляем созданнй объект в список
+        }
+        return groups;
     }
 }
--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.qa.training.addressbook.model.GroupData;
 
+import java.util.List;
+
 /**
  * Created by osoboleva on 20.09.2016.
  */
@@ -15,13 +17,13 @@ public class GroupModificationTests extends TestBase {
         if(! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().selectGroup(before - 1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test_new", null, null));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
