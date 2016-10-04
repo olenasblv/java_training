c8b2a686c5a71d2f9366142eb841464a7e5d2b65
Olena Soboleva <alenapoda@gmail.com>
2016-10-04 13:01:25 +0300

    Comparing lists of Contacts in Deletion test

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/ContactDeletionTests.java
@@ -24,5 +24,8 @@ public class ContactDeletionTests extends TestBase {
         app.getContactHelper().submitContactDeletion();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before,after);
     }
 }
