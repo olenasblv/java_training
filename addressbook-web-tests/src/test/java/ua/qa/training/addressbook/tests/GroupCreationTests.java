68163a77fa97068d5caec3f459daff8de117a1a4
Olena Soboleva <alenapoda@gmail.com>
2016-10-05 17:17:09 +0300

    Sorting group creation, modification,

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.qa.training.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +14,19 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().goToGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        GroupData group = new GroupData("test1", null, null);
+        GroupData group = new GroupData("test2", null, null);
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1); // сравниваем размеры списков
 
-        int max = 0; // устанавливаем макс на 0
-        for (GroupData g : after) { // сравниваем найденный максимум с id каждого элемента. Если id больше - увеличиваем макс. Меняем значение на новый найденный максимум
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        group.setId(max);
+
+        // новый способ
+        // список превращаем в поток, по нему пробегается функция сравниватель и находит макс элемент, сравниваются объекты типа groupData путем сравнения их идентификаторов
+        //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 }
