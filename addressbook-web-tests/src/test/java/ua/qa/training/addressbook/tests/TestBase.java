99faaf3bf6535ffba072b81b6e60b7b3e6bdf596
Olena Soboleva <alenapoda@gmail.com>
2016-10-06 12:53:39 +0300

    Added Before/AfterSuite instead of Befaore/AfterMethod

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/TestBase.java
@@ -1,8 +1,8 @@
 package ua.qa.training.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ua.qa.training.addressbook.appmanager.ApplicationManager;
 
 /**
@@ -10,14 +10,14 @@ import ua.qa.training.addressbook.appmanager.ApplicationManager;
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
