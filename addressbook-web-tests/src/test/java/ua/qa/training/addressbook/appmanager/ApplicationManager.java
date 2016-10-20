cad75fa71c213e52e6d43009471abd5586d09714
Olena Soboleva <alenapoda@gmail.com>
2016-10-20 18:19:40 +0300

    Configuration file

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/ApplicationManager.java
@@ -6,7 +6,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,53 +19,58 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
-  WebDriver wd;
+    private final Properties properties;
+    WebDriver wd;
 
-  private SessionHelper sessionHelper;
-  private NavigationHelper navigationHelper;
-  private ContactHelper contactHelper;
-  private GroupHelper groupHelper;
-  private String browser;
+    private SessionHelper sessionHelper;
+    private NavigationHelper navigationHelper;
+    private ContactHelper contactHelper;
+    private GroupHelper groupHelper;
+    private String browser;
 
-  public ApplicationManager(String browser) {
-    System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-    this.browser = browser;
-  }
-
-
-  public void init() {
-    if (Objects.equals(browser, BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (Objects.equals(browser, BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (Objects.equals(browser, BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+    public ApplicationManager(String browser) {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        this.browser = browser;
+        properties = new Properties();
     }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-
-    groupHelper = new GroupHelper(wd);
-    contactHelper = new ContactHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper = new SessionHelper(wd);
-
-    sessionHelper.login("admin", "secret");
-  }
 
 
-  public void stop() {
-    wd.quit();
-  }
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
 
-  public GroupHelper group() {
-    return groupHelper;
-  }
+        if (Objects.equals(browser, BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        } else if (Objects.equals(browser, BrowserType.CHROME)) {
+            wd = new ChromeDriver();
+        } else if (Objects.equals(browser, BrowserType.IE)) {
+            wd = new InternetExplorerDriver();
+        }
+        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
 
-  public ContactHelper contact() {
-    return contactHelper;
-  }
+        groupHelper = new GroupHelper(wd);
+        contactHelper = new ContactHelper(wd);
+        navigationHelper = new NavigationHelper(wd);
+        sessionHelper = new SessionHelper(wd);
 
-  public NavigationHelper goTo() {
-    return navigationHelper;
-  }
+        sessionHelper.login(properties.getProperty("web.adminLogin"),properties.getProperty("web.adminPassword"));
+    }
+
+
+    public void stop() {
+        wd.quit();
+    }
+
+    public GroupHelper group() {
+        return groupHelper;
+    }
+
+    public ContactHelper contact() {
+        return contactHelper;
+    }
+
+    public NavigationHelper goTo() {
+        return navigationHelper;
+    }
 }
