0753064687778f26ef3598067c6f7d4459d23c41
olenasblv <alenapoda@gmail.com>
2016-10-24 23:49:09 +0300

    ORM

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/appmanager/SessionHelper.java
@@ -2,7 +2,6 @@ package ua.qa.training.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by osoboleva on 9/19/2016.
