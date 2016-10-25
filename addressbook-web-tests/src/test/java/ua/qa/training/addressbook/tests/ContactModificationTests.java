e3fab7aeaa338ca2afc384560f8c662f3c39a1c8
olenasblv <alenapoda@gmail.com>
2016-10-26 00:20:22 +0300

    draft

--- addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ua/qa/training/addressbook/tests/ContactModificationTests.java
@@ -15,8 +15,8 @@ public class ContactModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().homePage();
-        if (app.contact().all().size() == 0) {
+        if (app.db().contacts().size() == 0) {
+            app.goTo().homePage();
             app.contact().create(new ContactData().withLastName("Last_name_test").withFirstName("First_name_test").withAddress("Address_test")
                     .withHomePhone("123456789").withEmail("email@mail.ru").withHomepage("homepage.com").withBirthdayDay(5)
                     .withBirthdayMonth("April").withBirthdayYear(1990).withGroup("test1"));
@@ -25,14 +25,14 @@ public class ContactModificationTests extends TestBase {
 
     @Test
     public void testContactModification() {
-        Contacts before = app.contact().all();
+        Contacts before = app.db().contacts();
         ContactData modifiedContact = before.iterator().next();
         ContactData contact = new ContactData().withId(modifiedContact.getId()).withLastName("Last_name_Mod").withFirstName("First_name_Mod").withAddress("Address_testMod")
                 .withHomePhone("123456789").withEmail("email@mail.ru").withHomepage("homepage.com").withBirthdayDay(5)
                 .withBirthdayMonth("April").withBirthdayYear(1990);
         app.contact().modify(contact);
         assertThat(app.contact().count(), equalTo(before.size()));
-        Contacts after = app.contact().all();
+        Contacts after = app.db().contacts();
         assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
     }
 }
