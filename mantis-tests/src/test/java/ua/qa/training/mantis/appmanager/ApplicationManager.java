a999b29db773a1697957a797b2d8a71df2d7622d
olenasblv <alenapoda@gmail.com>
2016-11-03 19:18:49 +0200

    MailHelper

--- mantis-tests/src/test/java/ua/qa/training/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ua/qa/training/mantis/appmanager/ApplicationManager.java
@@ -23,7 +23,7 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
-    private FtpHelper ftp;
+    //private FtpHelper ftp;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -58,12 +58,12 @@ public class ApplicationManager {
         return registrationHelper;
     }
 
-    public FtpHelper ftp() {
+    /*public FtpHelper ftp() {
         if (ftp == null) {
             ftp = new FtpHelper(this);
         }
         return ftp;
-    }
+    }*/
 
     public WebDriver getDriver() {
         if (wd == null) {
--- mantis-tests/src/test/java/ua/qa/training/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ua/qa/training/mantis/appmanager/RegistrationHelper.java
@@ -1,20 +1,20 @@
 package ua.qa.training.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
 /**
  * Created by osoboleva on 11/1/2016.
  */
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
     public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+        super(app);
     }
 
     public void start(String username, String email) {
         wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[value='Signup']"));
     }
 }
